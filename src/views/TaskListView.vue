90e0e4c214d748c2a133f5728204da43b628118e
kalidusachintha <kalidusachintha7@gmail.com>
2025-04-05 23:53:08 +0530

    Fixing styling issue with tasklist

--- src/views/TaskListView.vue
+++ src/views/TaskListView.vue
@@ -17,18 +17,20 @@ onMounted(() => {
       <h2 class="text-xl font-semibold text-gray-800">Your Tasks</h2>
     </div>
     <div class="bg-white rounded-xl shadow-md overflow-hidden">
-      <TaskItemView
-        v-for="task in taskStore.tasks"
-        :key="task.id"
-        :task="task"
-        :isDeleting="taskStore.deletingItem == task.id"
-      />
       <div
         v-if="taskStore.tasks.length === 0"
         class="bg-white p-8 rounded-lg shadow-sm border border-gray-200 text-center"
       >
         <IconSpinner v-if="taskStore.loading"> Loading..</IconSpinner>
         <p class="text-gray-600" v-else>You have no tasks yet. Create one to get started!</p>
+      </div>
+      <div v-else>
+        <TaskItemView
+          v-for="task in taskStore.tasks"
+          :key="task.id"
+          :task="task"
+          :isDeleting="taskStore.deletingItem == task.id"
+        />
       </div>
     </div>
   </div>
